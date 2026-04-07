19630df4ee6ca3fc9dd25c952c80ac4047b0386c
SzafirSzafirek <szafir.szafirek1@gmail.com>
2026-04-07 22:58:16 +0200

    Delete bigdiamond-11-najnowszy directory

--- bigdiamond-11-najnowszy/run-sql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-MYSQL="/Users/jakub/Library/Application Support/Local/lightning-services/mysql-8.0.35+4/bin/darwin-arm64/bin/mysql"
-SOCKET="/Users/jakub/Library/Application Support/Local/run/78YSqlpK-/mysql/mysqld.sock"
-"$MYSQL" --socket="$SOCKET" -u root -proot local -e "$1" 2>&1 | grep -v "Warning.*password"
--- bigdiamond-11-najnowszy/run-wp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-'/Users/jakub/Library/Application Support/Local/lightning-services/php-8.4.10+0/bin/darwin-arm64/bin/php' -c '/Users/jakub/Library/Application Support/Local/run/78YSqlpK-/conf/php/php.ini' /opt/homebrew/bin/wp --path=app/public "$@" 2>&1
